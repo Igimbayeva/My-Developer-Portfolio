b435bb3300c52080777b0c9c16363ff0e03a22b1
Igimbayeva <igimbayevadinara@gmail.com>
2024-07-15 01:49:48 -0400

    provided updates in the about section and revised the projects section with the links to the images

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,12 +10,14 @@ export const About = () => {
         <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
           <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
             <div class="md:5/12 lg:w-5/12">
-              <img src="/startup.png" alt="image" loading="lazy" width="" height=""/>
+              <img src="/me.jpg" alt="image" loading="lazy" width="" height=""/>
             </div>
             <div class="md:7/12 lg:w-6/12">
-              <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
-              <p class="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-              <p class="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+              <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Passionate Web Developer</h2>
+              <p class="mt-6 text-gray-600">
+              My name is Dinara, and I am a passionate web developer dedicated to creating seamless and engaging user experiences.</p>
+              <p class="mt-4 text-gray-600"> With a strong foundation in modern web technologies, I thrive on bringing creative ideas to life through clean, efficient, and well-documented code. Software engineering has been my dream career, and my journey in web development has been fueled by a deep curiosity and a commitment to continuous learning, allowing me to stay at the forefront of industry trends and best practices. I possess strong skills in Node, Express, JavaScript, SQL, NoSQL, ORM, MVC, React, MERN and STATE management. Whether I'm developing dynamic single-page applications or responsive websites, I take pride in crafting solutions that not only meet client needs but also delight users.</p>
+              <p class="mt-4 text-gray-600"> My enthusiasm for coding and problem-solving drives me to constantly push the boundaries of what's possible, making me a valuable asset to any development team.</p>
             </div>
           </div>
         </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,37 +6,37 @@ const projectData = [
         name: "Bootcamper Tech Blog (MVC)",
         github_url: "https://github.com/Igimbayeva/CU-FS-Bootcamper-Tech-Blog",
         deployed_url: "https://cu-fs-bootcamper-tech-blog.onrender.com",
-        img_url: "https://placehold.co/600x600"
+        img_url: "/blog.png"
     },
     {
         name: "NodeHR-Manager",
         github_url: "https://github.com/Igimbayeva/NodeHR-Manager",
         video_url: "https://drive.google.com/file/d/1sCLNN5CTOqBW9rh7qk706vJ_oEBhi9Ba/view",
-        img_url: "https://placehold.co/600x600"
+        img_url: "/HR.png"
     },
     {
         name: "SocialExpress-API (NoSQL, Express, Mongoose)",
         github_url: "https://github.com/Igimbayeva/SocialExpress-API",
         video_url: "https://drive.google.com/file/d/1njLpgNWG3Q0zP6UoBkTVh2Fjt3a_pqXD/view",
-        img_url: "https://placehold.co/600x600"
+        img_url: "social api.jpeg"
     },
     {
         name: "GRAPHQL-BOOKSHELF (MERN)",
         github_url: "https://github.com/Igimbayeva/GraphQL-Bookshelf",
         deployed_url: "https://graphql-bookshelf.render.com",
-        img_url: "https://placehold.co/600x600"
+        img_url: "/booksearch.png"
     }, 
     {
         name: "React2Give (React, Mern, Stack SPA)",
         github_url: "https://github.com/Igimbayeva/React2Give",
         deployed_url: "www.https://react2give.onrender.com",
-        img_url: "https://placehold.co/600x600"
+        img_url: "/react2give.png"
     },
     {
         name: "Weather Forecast Dashboard ",
         github_url: "https://github.com/Igimbayeva/Weather-Forecast-Dashboard",
         deployed_url: "https://igimbayeva.github.io/Weather-Forecast-Dashboard",
-        img_url: "https://placehold.co/600x600"
+        img_url: "/weather.png"
     }
 ]
 
