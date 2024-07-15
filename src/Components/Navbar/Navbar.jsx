ad2b439936d6c4ceb3aef0bfbf75ef8ddf0b8c9b
Igimbayeva <igimbayevadinara@gmail.com>
2024-07-15 03:04:07 -0400

    removed unused files and inserted personal logo into the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,63 +3,36 @@ import React from "react";
 export const Navbar = () => {
     return (
         <>
-           
-            <div class="bg-light">
-                <div class="container-fluid">
-                    <div class='d-flex justify-content-between align-items-center'>
-                        <div class="p-2"><a class="navbar-brand" href="/"><img src="https://htmljstemplates.com/static_files/images/logos/logo_5.png" height="90" alt="" /></a></div>
-                        <div class="p-2 text-end">
-                            <div class="d-inline-block mx-3 compact align-middle">
-                                {/* <span class="fs-3 fontStyle1">Call Us Today</span><br />
-                                <span class="fs-5 fontStyle2">1-800-324-1234</span> */}
-                            </div>
-                            <div class="d-inline-block mx-3 d-none d-lg-inline-block d-md-inline-block d-sm-inline-block">
-                                <a href="/contact">
-                                <button class="btn btn-outline-danger pt-2 px-4 compact">
-                                   
-                                    <span class="fontStyle1 fs-3"><i class="fas fa-pen-alt"></i> CONTACT ME</span>
+            <div className="container-fluid">
+                <div className='d-flex justify-content-between align-items-center'>
+                    <div className="p-2">
+                        <a className="navbar-brand" href="/">
+                            {/* Adjusted logo size */}
+                            <img src="/logo.png" height="30" alt="my logo" />
+                        </a>
+                    </div>
+                    <div className="p-2 text-end">
+                        <div className="d-inline-block mx-3 compact align-middle"></div>
+                        <div className="d-inline-block mx-3 d-none d-lg-inline-block d-md-inline-block d-sm-inline-block">
+                            <a href="/contact"> {/* Ensure this href points to your contact page */}
+                                <button className="btn btn-outline-danger pt-2 px-4 compact">
+                                    <span className="fontStyle1 fs-3"><i className="fas fa-pen-alt"></i> CONTACT ME</span>
                                 </button>
-                                </a>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <nav class="flex justify-between px-20 py-10 items-center bg-gradient-to-r from-red-400 to-red-700">
-                <h1 class="text-xl text-gray-100 font-bold">My Portfolio</h1>
-                <div class="flex items-center">
-        
-                    <ul class="flex items-center space-x-6">
-                        <a href="/"><li class="font-semibold text-gray-200">About</li></a>
-                        <a href="/projects"><li class="font-semibold text-gray-200">Projects</li></a>
-                        {/* <a href="/contact"><li class="font-semibold text-gray-200">Contact</li></a> */}
-                        <a href="/resume"><li class="font-semibold text-gray-200">Resume</li></a>
+            <nav className="flex justify-between px-20 py-10 items-center bg-gradient-to-r from-red-400 to-red-700">
+                <h1 className="text-xl text-gray-100 font-bold">My Portfolio</h1>
+                <div className="flex items-center">
+                    <ul className="flex items-center space-x-6">
+                        <a href="/"><li className="font-semibold text-gray-200">About</li></a>
+                        <a href="/projects"><li className="font-semibold text-gray-200">Projects</li></a>
+                        <a href="/resume"><li className="font-semibold text-gray-200">Resume</li></a>
                     </ul>
                 </div>
             </nav>
-            {/* <div class="bg-danger fontStyle2">
-                <button type="button" data-bs-toggle="collapse" data-bs-target="#bottomNavbar" class="btn btn-danger w-100 d-lg-none d-md-none d-sm-block d-xs-block rounded-0 p-2"><i class="fas fa-bars me-2"></i> Menu</button>
-                <div class="collapse navbar-collapse px-2" id="bottomNavbar">
-                    <div class="container-fluid">
-                        <div class="d-flex justify-content-between align-items-center" id="removeFlex">
-                            <div class="flex-grow-1">
-                                <a href="#" class="active d-inline-block customNav">Home</a>
-                                <a href="#" class="d-inline-block customNav">Services</a>
-                                <a href="#" class="d-inline-block customNav">Pricing</a>
-                                <a href="#" class="d-inline-block customNav">About Us</a>
-                                <a href="#" class="d-inline-block customNav">Partners</a>
-                                <a href="#" class="d-inline-block customNav">Blog</a>
-                                <a href="#" class="d-inline-block customNav">Contact Us</a>
-                            </div>
-                            <div class="m-3">
-                                <i class="fas fa-search text-white fs-6"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
- 
         </>
     )
 }
--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 
-
-export const ProjectCard = ({project}) => {
-    return (
-        <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
-            <div class="mb-8">
-                <img class="object-center object-cover rounded-full h-100 w-100" src={project.img_url} alt="photo" />
-            </div>
-            <div class="text-center">
-                <p class="text-xl text-gray-700 font-bold mb-2">{project.name}</p>
-             <a href={project.github_url}><p class="text-base text-gray-400 font-normal">GitHub URL</p></a> 
-             {
-                project.deployed_url ? 
-                (<a href={project.deployed_url}><p class="text-base text-gray-400 font-normal">Deployed URL</p></a> )
-                : null
-             }
-
-            {
-                project.video_url ? 
-                ( <a href={project.video_url}><p class="text-base text-gray-400 font-normal">Video Walkthrough URL</p></a>  )
-                : null
-             }
-            
-            </div>
-        </div>
-    )
-}
+export const ProjectCard = ({ project }) => {
+  return (
+    <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+      <div className="mb-8">
+        {/* Added border class and adjusted image dimensions */}
+        <img
+          className="object-center object-cover rounded-full h-60 w-100 border-5 border-red-600"
+          src={project.img_url}
+          alt="project thumbnail"
+        />
+      </div>
+      <div className="text-center">
+        <p className="text-xl text-gray-750 font-bold mb-2">{project.name}</p>
+        <a href={project.github_url}>
+          <p className="text-base text-red-600 font-normal">GitHub URL</p>
+        </a>
+        {project.deployed_url ? (
+          <a href={project.deployed_url}>
+            <p className="text-base text-red-600 font-normal">Deployed URL</p>
+          </a>
+        ) : null}
+        {project.video_url ? (
+          <a href={project.video_url}>
+            <p className="text-base text-red-600 font-normal">
+              Video Walkthrough URL
+            </p>
+          </a>
+        ) : null}
+      </div>
+    </div>
+  );
+};
